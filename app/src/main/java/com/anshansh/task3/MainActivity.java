f6269ca3bf863f72249befd537e7940d515c028b
YOUR NAME <mr.anshansh@gmail.com>
2020-05-01 17:54:20 +0530

    first commit

--- app/src/main/java/com/anshansh/task3/MainActivity.java
+++ app/src/main/java/com/anshansh/task3/MainActivity.java
@@ -1,14 +1,23 @@
 package com.anshansh.task3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar_layout);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("ATGChatApp");
     }
 }
