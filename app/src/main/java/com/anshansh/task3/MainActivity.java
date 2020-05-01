471810e230967d16115d7dc33b7d108dc5c32d44
YOUR NAME <mr.anshansh@gmail.com>
2020-05-01 21:51:37 +0530

    first commit

--- app/src/main/java/com/anshansh/task3/MainActivity.java
+++ app/src/main/java/com/anshansh/task3/MainActivity.java
@@ -9,20 +9,32 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
+//import static com.anshansh.task3.R.id.app_bar_layout;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private ViewPager viewPager;
     private TabLayout tabLayout;
+    private  TabsAccessAdapter tabsAccessAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.app_bar_layout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar_layout);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("ATGChatApp");
+
+        viewPager = (ViewPager) findViewById(R.id.app_view_pager);
+        tabsAccessAdapter = new TabsAccessAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabsAccessAdapter);
+
+
+        tabLayout = (TabLayout) findViewById(R.id.app_tab);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
---
+++ app/src/main/java/com/anshansh/task3/TabsAccessAdapter.java
@@ -0,0 +1,76 @@
+package com.anshansh.task3;
+
+import android.provider.ContactsContract;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabsAccessAdapter extends FragmentPagerAdapter {
+
+
+    /**public TabsAccessAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }*/
+
+    public TabsAccessAdapter(FragmentManager supportFragmentManager) {
+        super(supportFragmentManager);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position)
+        {
+            case 0:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+
+            case 1:
+                GroupsFragment groupsFragment = new GroupsFragment();
+                return groupsFragment;
+
+            case 2:
+                ContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        switch (position)
+        {
+            case 0:
+                //ChatsFragment chatsFragment = new ChatsFragment();
+                return "Chats";
+
+            case 1:
+                //GroupsFragment groupsFragment = new GroupsFragment();
+                return "Groups";
+
+            case 2:
+                //ContactsFragment contactsFragment = new ContactsFragment();
+                return "Contacts";
+
+            default:
+                return null;
+        }
+
+
+    }
+}
